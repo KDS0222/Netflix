fbbf373c9ba7fe1e49dcac76917d296e6a44679d
asok3861 <asok3861@naver.com>
2023-11-05 15:41:55 +0900

    검색페이지 검색기능 완성

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,6 +4,7 @@ import Wrapper from "./Wrapper";
 import Slider from "./Slider";
 import { useSearchParams } from "react-router-dom";
 import { SearchResult } from "./SearchResult";
+import Text from "./Text";
 
 export function Search() {
   let [searchParams, setSearchParams] = useSearchParams();
@@ -15,34 +16,41 @@ export function Search() {
   const movieFilterData = {};
   const tvFilterData = {};
 
-  const keyWord = searchParams.get('keyword');
-  
-
+  const keyWord = searchParams.get("keyword");
 
   Object.keys(movieData).map((key) => {
-    if (movieData[key].filter((v) => v.title && v.title.toLowerCase().trim().includes(keyWord)).length > 0) {
-      movieFilterData[key] = movieData[key].filter((v) => v.title && v.title.toLowerCase().trim().includes(keyWord))
+    if (
+      movieData[key].filter(
+        (v) => v.title && v.title.toLowerCase().trim().includes(keyWord)
+      ).length > 0
+    ) {
+      movieFilterData[key] = movieData[key].filter(
+        (v) => v.title && v.title.toLowerCase().trim().includes(keyWord)
+      );
     }
   });
 
   Object.keys(tvData).map((key) => {
-    if (tvData[key].filter((v) => v.name && v.name.toLowerCase().trim().includes(keyWord)).length > 0) {
-      tvFilterData[key] = tvData[key].filter((v) => v.name && v.name.toLowerCase().trim().includes(keyWord))
+    if (
+      tvData[key].filter(
+        (v) => v.name && v.name.toLowerCase().trim().includes(keyWord)
+      ).length > 0
+    ) {
+      tvFilterData[key] = tvData[key].filter(
+        (v) => v.name && v.name.toLowerCase().trim().includes(keyWord)
+      );
     }
   });
 
   let movieTemp = [];
   let tvTemp = [];
 
-  function total(){
-    
-    Object.keys(movieFilterData).map((key) => (
+  function total() {
+    Object.keys(movieFilterData).map((key) =>
       movieTemp.push(...movieFilterData[key])
-    ))
+    );
 
-    Object.keys(tvFilterData).map((key) => (
-      tvTemp.push(...tvFilterData[key])
-    ))
+    Object.keys(tvFilterData).map((key) => tvTemp.push(...tvFilterData[key]));
 
     const movieArray = movieTemp.filter((item, i) => {
       return (
@@ -51,7 +59,7 @@ export function Search() {
         }) === i
       );
     });
-    
+
     const tvArray = tvTemp.filter((item, i) => {
       return (
         tvTemp.findIndex((item2, j) => {
@@ -63,33 +71,27 @@ export function Search() {
     movieTemp = movieArray;
     tvTemp = tvArray;
   }
-  
+
   console.log(movieTemp);
   console.log(tvTemp);
-  
 
   total();
 
   // console.log(movieFilterData);
   // console.log(tvFilterData);
 
-  const filterKey = Object.keys(movieFilterData).filter((key, i) => movieFilterData[key][i] != null);
-
-
-
   // ! filterKey에서 topRated, upcoming 둘다 나와야하는데 topRated만 나오니까 이 부분 수정하면 검색부분은 끝난다.
   // ! tv쪽은 title이 아닌 name으로 변경한다.
 
   // const asd = Object.keys(filterKey).map(key => movieFilterData[filterKey[key]])
 
-
   // const filterKey = Object.keys(movieFilterData).filter((key, i) => {
   //   if (movieFilterData[key][i] != undefined) {
-  //     asd.push( 
+  //     asd.push(
   //       {
   //         key: movieFilterData[key]
   //       }
-        
+
   //     );
   //   }
   // });
@@ -97,14 +99,21 @@ export function Search() {
   return (
     <>
       <Wrapper
-      padding="70px 0 0 0"
+        padding="70px 0 0 0"
         height="100%"
         justify="center"
         align="center"
       >
-        <SearchResult movieData={movieTemp}>영화 검색결과</SearchResult>
-        <SearchResult tvData={tvTemp} >TV 검색결과</SearchResult>
-
+        {movieTemp.length > 0 || tvTemp.length > 0 ? (
+          <>
+            <SearchResult movieData={movieTemp}>영화 검색결과</SearchResult>
+            <SearchResult tvData={tvTemp}>TV 검색결과</SearchResult>
+          </>
+        ) : (
+          <Wrapper height="calc(100vh - 70px)" display="flex" justify="center" align="center">
+            <Text size="32px" weight="bold">검색결과가 없습니다.</Text>
+          </Wrapper>
+        )}
       </Wrapper>
     </>
   );
